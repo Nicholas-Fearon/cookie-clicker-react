1cc2420ec8b7e524b78ca1e1797a89cb3722df6f
Nicholas-Fearon <nejfearon@gmail.com>
2024-11-05 10:57:20 +0000

    upgrades edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
-import Clicker from "./components/Clicker";
 import Upgrades from "./components/Upgrades";
 function App() {
   return (
     <>
-      <Upgrades />
+      
       <Header />
       <div className="card">
-        <Clicker />
       </div>
+      <Upgrades />
     </>
   );
 }
--- src/components/Upgrades.jsx
+++ src/components/Upgrades.jsx
@@ -1,27 +1,46 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("https://znbnodscuqgrehrjiojy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuYm5vZHNjdXFncmVocmppb2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2NzAxNzQsImV4cCI6MjA0NTI0NjE3NH0.qfVU54KAsDRZLrZK9oZ4YNlExGdsRYWBUmOCbD3SIWU");
-
+const supabase = createClient(
+  "https://znbnodscuqgrehrjiojy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuYm5vZHNjdXFncmVocmppb2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2NzAxNzQsImV4cCI6MjA0NTI0NjE3NH0.qfVU54KAsDRZLrZK9oZ4YNlExGdsRYWBUmOCbD3SIWU"
+);
 
 export default function Upgrades() {
+  const [upgrades, setUpgrades] = useState([]);
+  const [cookies, setCookies] = useState(0);
 
-        const [upgrades, setUpgrades] = useState([]);
-    
-        useEffect(() => {
-          getUpgrades();
-        }, []);
-    
-        async function getUpgrades() {
-          const { data } = await supabase.from("upgrades").select();
-          setUpgrades(data);
-        }
-    
-        return (
-          <ul>
-            {upgrades.map((upgrade) => (
-              <li key={upgrade.upgrade}>{upgrade.upgrade}</li>
-            ))}
-          </ul>
-        );
-      }
+  useEffect(() => {
+    getUpgrades();
+  }, []);
+
+  async function getUpgrades() {
+    const { data } = await supabase.from("upgrades").select();
+    setUpgrades(data);
+    console.log(data);
+  }
+
+  function handleUpgrades(upgrade) {
+    console.log("Handle grades");
+    console.log(cookies);
+
+    setCookies(cookies - upgrade.cost);
+  }
+
+  return (
+    <>
+      <p>Cookie count = {cookies}</p>
+      <p>Cookies per sec = </p>
+
+      <button onClick={() => setCookies((cookies) => cookies + 1)}>
+        Cookies
+      </button>
+
+      {upgrades.map((upgrade) => (
+        <button key={upgrade.upgrade} onClick={() => handleUpgrades(upgrade)}>
+          {upgrade.upgrade} - {upgrade.cost}
+        </button>
+      ))}
+    </>
+  );
+}
